3f7693295e5d56a853c3ebaba9de009713c61c3a
Marcos V <67884162+usmarcos@users.noreply.github.com>
2022-11-24 19:16:04 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import menu.Menu;
 public class Main {
     public static void main(String[] args) {
         /**
-         * metodos.Jogada da Velha, para conclusão da etapa de introdução a java
+         * Jogo da Velha, para conclusão da etapa de introdução a java
          */
         System.out.println("# Jogo da Velha #");
         new Menu().menu();
