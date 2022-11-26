7fb99be4ab274031b92c465cbf743e9847a5bf0d
Samir <samircd9@gmail.com>
2022-11-26 17:51:46 -0300

    Alterações no projeto, tratamento de erros

---
+++ jogo-da-velha/src/JogoDaVelha.java
@@ -0,0 +1,12 @@
+public class JogoDaVelha {
+    private int linha;
+    private int coluna;
+
+    public void setLinha(int linha) {
+        this.linha = linha;
+    }
+
+    public void setColuna(int coluna) {
+        this.coluna = coluna;
+    }
+}
--- jogo-da-velha/src/Main.java
+++ jogo-da-velha/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         String tabuleiro[][] = new String[3][3];
-        String jogador1 = " X |", jogador2 = " O |", jogador = "";
+        String jogador1 = " X ", jogador2 = " O ", jogador = "";
 
         System.out.println("Bem vindo ao jogo da velha\nEntre com numeros de 1 a 3 para escolher as posições\n");
         System.out.println("Informe quantas partidas deseja jogar: ");
@@ -23,7 +23,7 @@ public class Main {
             boolean ganhador = false;
             boolean validaPosicao = true;
 
-            while (contador <= 10 && numeroDePartidas > 0) {
+            while (contador <= 9 && numeroDePartidas > 0) {
 
                 if (verifica) {
                     jogador = jogador1;
@@ -40,8 +40,10 @@ public class Main {
                 validaPosicao = validaJogada(tabuleiro, jogadaLinha, jogadaColuna);
 
                 while (validaPosicao == true) {
-                    System.out.println("Posição já foi marcada. Escolha outra posição: ");
+                    System.out.println("Posição já foi marcada ou não existe. Escolha outra posição: ");
+                    System.out.println("Jogador escolha o número da linha: ");
                     jogadaLinha = scanner.nextShort();
+                    System.out.println("Jogador escolha o número da coluna: ");
                     jogadaColuna = scanner.nextShort();
                     validaPosicao = validaJogada(tabuleiro, jogadaLinha, jogadaColuna);
                 }
@@ -52,10 +54,10 @@ public class Main {
 
                 if (contador >= 5) {
                     ganhador = verificaVencedor(tabuleiro, jogador);
-                    if (ganhador && jogador.equals(" X |")) {
+                    if (ganhador && jogador.equals(" X ")) {
                         pontosJogador1++;
                         System.out.println("Jogador1, você venceu!");
-                    } else if (ganhador && jogador.equals(" O |")) {
+                    } else if (ganhador && jogador.equals(" O ")) {
                         pontosJogador2++;
                         System.out.println("Jogador2, você venceu!");
                     }
@@ -67,17 +69,42 @@ public class Main {
                         limpaTabuleiro(tabuleiro);
                         exibeTabuleiro(tabuleiro);
                         ganhador = false;
-                        contador = 1;
+                        contador = 0;
                         numeroDePartidas--;
                     }
                 }
 
-                if (pontosJogador1 == pontosJogador2 && numeroDePartidas == 0) {
-                    System.out.println("O número de partidas acabou em empate, para ter um vencedor, jogue mais uma vez!");
-                    ganhador = false;
-                    contador = 1;
-                    numeroDePartidas++;
+                if (pontosJogador1 == pontosJogador2 && contador == 9 && numeroDePartidas == 1) {
+                    contador = 0;
+                    limpaTabuleiro(tabuleiro);
+                    exibeTabuleiro(tabuleiro);
                 }
+
+                if (numeroDePartidas != 1 && contador == 9) {
+                    System.out.println("A rodada ficou empatada!");
+                    contador = 0;
+                    numeroDePartidas--;
+                    limpaTabuleiro(tabuleiro);
+                    exibeTabuleiro(tabuleiro);
+                }
+
+
+//                if (pontosJogador1 == pontosJogador2 && numeroDePartidas == 0) {
+//                    System.out.println("O número de partidas acabou em empate, para ter um vencedor, jogue mais uma vez!");
+//                    ganhador = false;
+//                    contador = 1;
+//                    numeroDePartidas++;
+//                }
+//                else if (pontosJogador1 == pontosJogador2 && ganhador == false) {
+//                    System.out.println("Primeira partida deu empate");
+//                    System.out.println("Placar do jogo:\n" +
+//                            "Jogador1: " + pontosJogador1 + "\n" +
+//                            "Jogador2: " + pontosJogador2);
+//                    limpaTabuleiro(tabuleiro);
+//                    exibeTabuleiro(tabuleiro);
+//                    contador = 1;
+//                    numeroDePartidas--;
+//                }
 
                 contador++;
             }
@@ -88,9 +115,19 @@ public class Main {
     }
 
     public static boolean validaJogada(String[][] tabuleiro, short jogadaLinha, short jogadaColuna) {
-        if (tabuleiro[jogadaLinha - 1][jogadaColuna -1].contains("X") || tabuleiro[jogadaLinha - 1][jogadaColuna -1].contains("O")) {
+        try {
+            if (jogadaLinha < 1 || jogadaLinha > 3 || jogadaColuna < 1 || jogadaColuna > 3) {
+                throw new PosicaoInvalidaException();
+            }
+
+            if (tabuleiro[jogadaLinha - 1][jogadaColuna -1].contains("X") || tabuleiro[jogadaLinha - 1][jogadaColuna -1].contains("O")) {
+                throw new PosicaoInvalidaException();
+            }
+        } catch (PosicaoInvalidaException e) {
+            System.out.println("Caiu na exceção de posição inválida");
             return true;
         }
+
         return false;
     }
 
@@ -98,6 +135,9 @@ public class Main {
         for (int linha = 0; linha < tabuleiro.length; linha++) {
             for (int coluna = 0; coluna < tabuleiro[linha].length; coluna++) {
                 System.out.print(tabuleiro[linha][coluna]);
+                if (coluna < (tabuleiro.length - 1)) {
+                    System.out.print("|");
+                }
             }
             System.out.println();
         }
@@ -106,7 +146,7 @@ public class Main {
     public static void limpaTabuleiro(String[][] tabuleiro) {
         for (int i = 0; i < tabuleiro.length; i++) {
             for (int j = 0; j < tabuleiro[i].length; j++) {
-                tabuleiro[i][j] = "   |";
+                tabuleiro[i][j] = "   ";
             }
         }
     }
